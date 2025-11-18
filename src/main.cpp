06aeb3e0f5141c34a296f9eef9ecccd554cad365
orchistro <orchistro@gmail.com>
2025-11-18 23:44:36 +0900

    use {fmt} to print vectors

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,36 @@
-#include <iostream>
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+
 #include <unifex/on.hpp>
 #include <unifex/single_thread_context.hpp>
 #include <unifex/sync_wait.hpp>
 #include <unifex/task.hpp>
+#include <vector>
 
-constexpr int expected = 42;
+auto expected = std::vector<int>{1, 2, 3, 4, 5};
 
 int main() {
   auto ctx1 = unifex::single_thread_context{};
   auto ctx2 = unifex::single_thread_context{};
 
-  auto f1 = [&]() -> unifex::task<int> {
+  auto f1 = [&]() -> unifex::task<decltype(expected)> {
     co_await unifex::schedule(ctx1.get_scheduler());
     co_return expected;
   };
 
-  auto f2 = [&]() -> unifex::task<int> { co_return co_await f1(); };
+  auto f2 = [&]() -> unifex::task<decltype(expected)> {
+    co_return co_await f1();
+  };
 
   auto result = unifex::sync_wait(unifex::on(ctx2.get_scheduler(), f2()));
   if (!result.has_value()) {
     throw "result has no value";
   }
+
   if (result.value() == expected) {
-    std::cout << "as expected" << std::endl;
+    fmt::println("expected:{}", expected);
+    fmt::println("result.value():{}", result.value());
+    fmt::println("ok");
   } else {
     throw "result is not as expected";
   }
